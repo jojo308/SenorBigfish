41790ab90db020ce3b7ef169f60afe89ee6a39c8
jojo308 <joostsimons822@gmail.com>
2020-07-31 02:28:46 +0200

    added spawn chance and loot drop for senor bigfish

--- NPCs/SenorBigfish.cs
+++ NPCs/SenorBigfish.cs
@@ -8,7 +8,7 @@ namespace SenorBigfish.NPCs
     {
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("SenorBigfish");
+            DisplayName.SetDefault("Senor Bigfish");
             Main.npcFrameCount[npc.type] = 1;
         }
 
@@ -19,12 +19,55 @@ namespace SenorBigfish.NPCs
             npc.aiStyle = 3; // fighter AI
             npc.damage = 7;
             npc.defense = 10;
-            npc.lifeMax = 125;
+            npc.lifeMax = 300;
             npc.HitSound = SoundID.NPCHit1;
             npc.DeathSound = SoundID.NPCDeath1;
             npc.value = 25f;
             npc.buffImmune[BuffID.Poisoned] = true;
             npc.buffImmune[BuffID.Confused] = false;
+        }
+
+        public override float SpawnChance(NPCSpawnInfo spawnInfo)
+        {
+            if (Main.hardMode)
+            {
+                return SpawnChanceHelper(spawnInfo);
+            }
+            else
+            {
+                return 0f; // can't spawn until world is in harmode
+            }
+        }
+
+        /*
+         * Helper method for SpawnChance(), determines the spawnrate based on the given spawnInfo.
+         * This NPC spawns naturally in the ocean biome or on the entire surface if it's raining.
+         * */
+        private float SpawnChanceHelper(NPCSpawnInfo spawnInfo)
+        {
+            if (spawnInfo.player.ZoneBeach)
+            {
+                return 0.2f;
+            }
+            else if (Main.raining)
+            {
+                return 0.1f;
+            }
+            else if (Main.bloodMoon)
+            {
+                return 0.2f;
+            }
+            return 0;
+        }
+
+        public override void NPCLoot()
+        {
+            var rect = npc.getRect();
+            if (Main.rand.NextFloat() < .01f)
+            {
+                Item.NewItem(rect.X, rect.Y, rect.Width, rect.Height, mod.ItemType("SenorBubbles"), 1);
+            }
+            base.NPCLoot();
         }
     }
 }
